a892b0e7803dd955e51f7f5191bd7e8d0a13f74a
jamiahuswalton <jamiahuswalton@gmail.com>
2016-03-26 16:30:37 -0500

    General Chagnes

---
+++ app/src/main/java/lywalton_portfolio/httpbit/fragmentui/MainActivity.java
@@ -0,0 +1,43 @@
+package lywalton_portfolio.httpbit.fragmentui;
+
+import android.app.Fragment;
+import android.preference.PreferenceActivity;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.FrameLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (findViewById(R.id.fragment_container) !=null)
+        {
+            // However, if we're being restored from a previous state,
+            // then we don't need to do anything and should return or else
+            // we could end up with overlapping fragments.
+            if (savedInstanceState != null) {
+                return;
+            }
+
+            // Create a new Fragment to be placed in the activity layout
+            ItemList firstFragment = new ItemList();
+
+            // In case this activity was started with special instructions from an
+            // Intent, pass the Intent's extras to the fragment as arguments
+            firstFragment.setArguments(getIntent().getExtras());
+
+            // Add the fragment to the 'fragment_container' FrameLayout
+            getSupportFragmentManager().beginTransaction().add(R.id.fragment_container,firstFragment).commit();
+
+
+        }
+
+
+
+    }
+}
