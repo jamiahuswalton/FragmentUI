8dd72af9c4070a657beee466c482ad8a6beb7e92
jamiahuswalton <jamiahuswalton@gmail.com>
2016-03-26 17:32:37 -0500

    I was able to successfully allow the button to change from one fragment to another.

--- app/src/main/java/lywalton_portfolio/httpbit/fragmentui/MainActivity.java
+++ app/src/main/java/lywalton_portfolio/httpbit/fragmentui/MainActivity.java
@@ -1,20 +1,32 @@
 package lywalton_portfolio.httpbit.fragmentui;
 
 import android.app.Fragment;
+import android.content.pm.ActivityInfo;
 import android.preference.PreferenceActivity;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.Toast;
+
+import static android.content.pm.ActivityInfo.SCREEN_ORIENTATION_PORTRAIT;
 
 public class MainActivity extends AppCompatActivity {
+
+    boolean FistToSecond = true; //True = First to second, False = Second
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        setRequestedOrientation(SCREEN_ORIENTATION_PORTRAIT);
+
+        /*
         if (findViewById(R.id.fragment_container) !=null)
         {
             // However, if we're being restored from a previous state,
@@ -26,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
 
             // Create a new Fragment to be placed in the activity layout
             ItemList firstFragment = new ItemList();
+            FacePicture secondFragment = new FacePicture();
 
             // In case this activity was started with special instructions from an
             // Intent, pass the Intent's extras to the fragment as arguments
@@ -36,8 +49,38 @@ public class MainActivity extends AppCompatActivity {
 
 
         }
+        */
+
+        Button butSwitchFragments = (Button) findViewById(R.id.button_switchFragements);
+
+        assert butSwitchFragments != null;
+        butSwitchFragments.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (FistToSecond)
+                {
+                    FacePicture secondFragment = new FacePicture();
+                    FragmentTransaction newTransaction = getSupportFragmentManager().beginTransaction();
+
+                    newTransaction.replace(R.id.fragment_container,secondFragment);
+                    newTransaction.addToBackStack(null);
+                    newTransaction.commit();
 
 
+                    FistToSecond = false;
+                }
+                else if(!FistToSecond)
+                {
+                    ItemList firstFragment = new ItemList();
+                    FragmentTransaction newTransaction = getSupportFragmentManager().beginTransaction();
 
+                    newTransaction.replace(R.id.fragment_container,firstFragment);
+                    newTransaction.addToBackStack(null);
+                    newTransaction.commit();
+
+                    FistToSecond = true;
+                }
+            }
+        });
     }
 }
